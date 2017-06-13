9d185f8b024fe609d92801e364c91c42a2da1394
Björn Dahlgren <bjodah@gmail.com>
2017-06-13 19:21:39 +0200

    Fix nbconvert --to arg

--- bin/render_notebooks.sh
+++ bin/render_notebooks.sh
@@ -4,5 +4,5 @@ python setup.py install
 mkdir deploy
 cd deploy
 jupyter nbconvert --debug --to=html --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 ../notebooks/*.ipynb
-jupyter nbconvert --debug --to=ipynb --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 ../notebooks/*.ipynb
+jupyter nbconvert --debug --to=notebook --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 ../notebooks/*.ipynb
 cd -
