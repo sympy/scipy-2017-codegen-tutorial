1655ff2c60c6982a59896067b008d99b373af218
Björn Dahlgren <bjodah@gmail.com>
2017-06-13 20:11:30 +0200

    pip install editable for now during CI runs

--- bin/render_notebooks.sh
+++ bin/render_notebooks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 source activate codegen17
-python setup.py install
+python -m pip install -e .
 for f in notebooks/{30,35,40,45,50}*.ipynb; do
     jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=html $f
     jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=notebook $f
