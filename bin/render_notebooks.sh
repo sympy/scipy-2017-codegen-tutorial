8526665f706742143d9abecc72ccf9eb32072375
Björn Dahlgren <bjodah@gmail.com>
2017-06-14 03:13:01 +0200

    Debugging AppVeyor & CircleCI

--- bin/render_notebooks.sh
+++ bin/render_notebooks.sh
@@ -2,8 +2,8 @@
 source activate codegen17
 python -m pip install -e .
 for f in notebooks/{30,35,40,45,50}*.ipynb; do
-    jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=html $f
-    jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=notebook $f
+    python -m nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=html $f
+    python -m nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=notebook $f
 done
 mkdir deploy
 mv notebooks/*.html deploy/
