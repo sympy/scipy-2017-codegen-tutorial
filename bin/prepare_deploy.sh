6804bcd20fc434cc9ed5313c190a25a274cefefa
Björn Dahlgren <bjodah@gmail.com>
2017-06-22 19:09:20 +0200

    Fix bin/prepare_deploy.sh

--- bin/prepare_deploy.sh
+++ bin/prepare_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir deploy
+sed -i.bak 's/.ipynb/.html/' index.ipynb
 jupyter nbconvert --to=html index.ipynb
 cp -r index.* intro-slides/ notebooks/ deploy/
 sed -i.bak "s/number: 0/number: ${DRONE_BUILD_NUMBER}/" conda-recipe/meta.yaml
