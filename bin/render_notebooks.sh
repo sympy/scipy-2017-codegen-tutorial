cdb09f5192fbfa869458518705b24932eeb241c0
Björn Dahlgren <bjodah@gmail.com>
2017-06-22 15:58:45 +0200

    CI fixes

--- bin/render_notebooks.sh
+++ bin/render_notebooks.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
-sed -i 's/conda-root-py/python3/' notebooks/*.ipynb
 source activate codegen17
 python -m pip install https://github.com/sympy/sympy/archive/master.tar.gz  # rust printer
 python setup.py develop
@@ -7,6 +6,7 @@ for f in notebooks/*.ipynb; do
     if [[ $f == notebooks/_* ]]; then
         continue
     fi
+    sed -i 's/conda-root-py/python3/' $f
     jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=html $f
     jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=notebook $f
 done
