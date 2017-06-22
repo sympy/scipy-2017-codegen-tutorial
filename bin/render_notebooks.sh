2a66cad7bf915a342c27c1aeb526b0f9a6c745c7
Björn Dahlgren <bjodah@gmail.com>
2017-06-22 17:59:25 +0200

    CI fixes

--- bin/render_notebooks.sh
+++ bin/render_notebooks.sh
@@ -6,7 +6,7 @@ for f in notebooks/*.ipynb; do
     if [[ $f == notebooks/_* ]]; then
         continue
     fi
-    sed -i 's/conda-root-py/python3/' $f
+    sed -i.bak 's/conda-root-py/python3/' $f
     jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=html $f
     jupyter nbconvert --debug --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=300 --to=notebook $f
 done
