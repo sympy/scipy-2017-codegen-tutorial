8a8be1d8a75926405e9e5f6553ef26188156212a
Björn Dahlgren <bjodah@gmail.com>
2017-06-28 17:04:55 +0200

    C_INCLUDE_PATH -> pyxbld (cross-platform)

--- scipy2017codegen/templates.py
+++ scipy2017codegen/templates.py
@@ -14,3 +14,16 @@ sundials = {
     os.path.basename(pth): open(pth).read() for pth in glob.glob(
         os.path.join(sundials_templates_dir, '*.*'))
 }
+
+def render_pyxbld(pyxbasename, **kwargs):
+    if 'include_dirs' not in kwargs:
+        kwargs['include_dirs'] = []
+    if 'library_dirs' not in kwargs:
+        kwargs['library_dirs'] = []
+    if 'libraries' not in kwargs:
+        kwargs['libraries'] = []
+    if 'extra_compile_args' not in kwargs:
+        kwargs['extra_compile_args'] = []
+    if 'extra_link_args' not in kwargs:
+        kwargs['extra_link_args'] = []
+    open(pyxbasename + '.pyxbld', 'wt').write(pyxbld % kwargs)
