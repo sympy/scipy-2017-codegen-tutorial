636e36673e36e15aec441f6dd348c9175214ed36
Jason K. Moore <moorepants@gmail.com>
2017-07-06 22:09:28 -0700

    Use SymPy 1.1 from conda-forge.

--- bin/render_notebooks.sh
+++ bin/render_notebooks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -xe
 source activate codegen17
-python -m pip install https://github.com/sympy/sympy/archive/sympy-1.1rc1.tar.gz
 python setup.py develop
 for f in notebooks/*.ipynb; do
     if [[ $f == notebooks/_* ]]; then
