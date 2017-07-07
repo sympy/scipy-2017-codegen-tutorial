0b08c085c2b656afac24d6f8e743446bcffb8c58
Jason K. Moore <moorepants@gmail.com>
2017-07-06 22:55:00 -0700

    Made the conda check work in environments. Fixes #21.

--- test_installation.py
+++ test_installation.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
 from sys import exit
+from subprocess import check_output
+
+try:
+    from pkg_resources import parse_version
+except ImportError:
+    print("Setuptools must be installed for the tutorial")
 
 try:
     import sympy
 except ImportError:
     print("SymPy must be installed for the tutorial")
 
-if sympy.__version__ != '1.1':
-    print("SymPy 1.1 is required for the tutorial. Note SymPy 1.1 will be released before July 10.")
+if parse_version(sympy.__version__) < parse_version('1.1'):
+    print("SymPy 1.1 is required for the tutorial.")
 
 try:
     import numpy
@@ -37,13 +43,12 @@ except:
     raise
 
 try:
-    import conda
-except ImportError:
+    s = check_output(['conda', '--version'])
+except FileNotFoundError:
     print("conda is needed (either anaconda or miniconda from https://www.continuum.io/downloads)")
     print("(try rerunning this script under conda if you are using for system's python distribution)")
 else:
-    major, minor, patch = map(int, conda.__version__.split('.'))
-    if major > 4 or (major == 4 and minor >= 1):
+    if parse_version(s.decode('utf-8').strip()[6:]) >= parse_version('4.1.0'):
         pass
     else:
         print("please update conda ($ conda update conda), we need conda >= 4.1.0")
