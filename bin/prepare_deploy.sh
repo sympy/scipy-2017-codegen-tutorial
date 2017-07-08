afae691740262df093623912d6c48d000b6fc80d
Aaron Meurer <asmeurer@gmail.com>
2017-07-08 16:12:49 -0400

    Fix cp command for Mac

--- bin/prepare_deploy.sh
+++ bin/prepare_deploy.sh
@@ -4,7 +4,7 @@ mkdir -p deploy/notebooks
 sed -i.bak 's/ipynb/html/' index.ipynb
 jupyter nbconvert --to=html index.ipynb
 mv index.ipynb.bak index.ipynb
-cp -r index.* intro-slides/ notebooks/ deploy/
+cp -R index.* intro-slides/ notebooks/ deploy/
 if [[ "$DRONE" == "true" ]]; then
     sed -i.bak "s/number: 0/number: ${DRONE_BUILD_NUMBER}/" conda-recipe/meta.yaml
 fi
