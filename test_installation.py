ecc98e995e67946b07e4f69808984567071bd95c
Jason K. Moore <moorepants@gmail.com>
2017-07-09 09:00:44 -0700

    Improved the test_installation.py script. Trying to address #24.

--- test_installation.py
+++ test_installation.py
@@ -6,15 +6,7 @@ from subprocess import check_output
 try:
     from pkg_resources import parse_version
 except ImportError:
-    print("Setuptools must be installed for the tutorial")
-
-try:
-    import sympy
-except ImportError:
-    print("SymPy must be installed for the tutorial")
-
-if parse_version(sympy.__version__) < parse_version('1.1'):
-    print("SymPy 1.1 is required for the tutorial.")
+    print("Setuptools must be installed for the tutorial.")
 
 try:
     import numpy
@@ -27,20 +19,27 @@ except ImportError:
     print("Cython is required for the tutorial")
 
 try:
-    import scipy
+    import sympy
 except ImportError:
-    print("scipy is required for the tutorial")
-
-from sympy.utilities.autowrap import ufuncify
-from sympy.abc import x
-from sympy import sin
+    print("SymPy must be installed for the tutorial")
+else:
+    if parse_version(sympy.__version__) < parse_version('1.1'):
+        print("SymPy >= 1.1 is required for the tutorial.")
+    from sympy.utilities.autowrap import autowrap
+    from sympy.abc import x
+    from sympy import sin
+    try:
+        f = autowrap(sin(x), backend='cython')
+        assert f(0) == 0
+    except:
+        msg = ("sympy.utilities.autowrap.autowrap does not work, your Cython "
+               "installation or compiler may be missing")
+        print(msg)
 
 try:
-    f = ufuncify(x, sin(x))
-    assert f(0) == 0
-except:
-    print("sympy.utilities.autowrap.ufuncify does not work, your Cython installation or compiler may be missing")
-    raise
+    import scipy
+except ImportError:
+    print("Scipy is required for the tutorial")
 
 try:
     s = check_output(['conda', '--version'])
@@ -48,10 +47,11 @@ except FileNotFoundError:
     print("conda is needed (either anaconda or miniconda from https://www.continuum.io/downloads)")
     print("(try rerunning this script under conda if you are using for system's python distribution)")
 else:
-    if parse_version(s.decode('utf-8').strip()[6:]) >= parse_version('4.1.0'):
-        pass
-    else:
-        print("please update conda ($ conda update conda), we need conda >= 4.1.0")
+    installed_version = s.decode('utf-8').strip().split(' ')[-1]
+    if parse_version(installed_version) < parse_version('4.1.0'):
+        msg = ("You have conda {} installed. The tutorial requires "
+               "conda >= 4.1.0. Please update conda ($ conda update conda).")
+        print(msg.format(installed_version))
         exit(1)
 
 try:
