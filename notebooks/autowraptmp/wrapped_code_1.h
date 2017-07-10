96b1aa0cc4637382709795a11c749ba1a2d53574
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2017-07-10 05:17:45 +0000

    Update docs after building Travis build 211 of sympy/scipy-2017-codegen-tutorial
    The docs were built from the branch 'master' against the commit
    ee71a32d5c94959442c3f42fcfbcbb39d398f1bc.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/sympy/scipy-2017-codegen-tutorial/jobs/251875446.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/bin/doctr deploy . --built-docs deploy/

--- notebooks/autowraptmp/wrapped_code_1.h
+++ notebooks/autowraptmp/wrapped_code_1.h
@@ -10,7 +10,7 @@
 #ifndef AUTOWRAP__WRAPPED_CODE_1__H
 #define AUTOWRAP__WRAPPED_CODE_1__H
 
-void autofunc(double *y, double *out_9173660151346531389);
+void autofunc(double *y, double *out_1200047534545697034);
 
 #endif
 
--- notebooks/autowraptmp/wrapper_module_1.pyx
+++ notebooks/autowraptmp/wrapper_module_1.pyx
@@ -2,10 +2,10 @@ import numpy as np
 cimport numpy as np
 
 cdef extern from 'wrapped_code_1.h':
-    void autofunc(double *y, double *out_9173660151346531389)
+    void autofunc(double *y, double *out_1200047534545697034)
 
 def autofunc_c(np.ndarray[np.double_t, ndim=2] y):
 
-    cdef np.ndarray[np.double_t, ndim=2] out_9173660151346531389 = np.empty((14,14))
-    autofunc(<double*> y.data, <double*> out_9173660151346531389.data)
-    return out_9173660151346531389
+    cdef np.ndarray[np.double_t, ndim=2] out_1200047534545697034 = np.empty((14,14))
+    autofunc(<double*> y.data, <double*> out_1200047534545697034.data)
+    return out_1200047534545697034
